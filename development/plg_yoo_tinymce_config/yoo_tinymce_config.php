d2380bab86aba66e56c7127b12c84ecdb0b4bc28
jbjhjm <j_mewes@web.de>
2017-05-23 16:55:49 +0200

    Update 0.1.1 - plugin supports css file located at templates > [default Template] > css > tinymce.css now.

--- development/plg_yoo_tinymce_config/yoo_tinymce_config.php
+++ development/plg_yoo_tinymce_config/yoo_tinymce_config.php
@@ -13,6 +13,11 @@ class plgSystemYoo_tinymce_config extends JPlugin
 
 		if($app->isAdmin()) {
 
+			$db = JFactory::GetDbo();
+			$db->setQuery('SELECT template FROM #__template_styles WHERE client_id=0 AND home=1');
+			$defaultTmpl = $db->loadResult();
+
+			$templatePath = '/templates/'.$defaultTmpl.'/';
 			$assetsPath = '/plugins/system/yoo_tinymce_config/assets/';
 
 			$customSetup = "{}";
@@ -21,18 +26,27 @@ class plgSystemYoo_tinymce_config extends JPlugin
 				if(!is_object(json_decode($customSetup))) $customSetup = "{_error:'invalid json data!'}";
 			}
 
-			$js = "
+			if(file_exists(JPATH_ROOT . $templatePath.'css/tinymce.css')) {
+				$cssFile = $templatePath.'css/tinymce.css';
+			} else if(file_exists(JPATH_ROOT . $assetsPath.'tinymce.css')) {
+				$cssFile = $assetsPath.'tinymce.css';
+			} else {
+				$cssFile = false;
+			}
+			if($cssFile) $cssFile = 'content_css : "'.$cssFile.'",';
 
+
+			$js = "
 			var tinyMCEoptions = Joomla.getOptions('plg_editor_tinymce');
 			if(tinyMCEoptions) {
 				tinyMCEoptions.tinyMCE.default = jQuery.extend(tinyMCEoptions.tinyMCE.default,{
-					content_css : '{$assetsPath}tinymce.css',
+					{$cssFile}
 					plugins : 'autolink,lists,save,colorpicker,link,code,paste,wordcount,autosave,contextmenu',
 					preview_styles : 'font-family font-size font-weight font-style text-decoration text-transform color',
 				},{$customSetup});
 			}
-
 			";
+
 
 			JFactory::GetDocument()->addScriptDeclaration($js);
 		}
